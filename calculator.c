f752b021043f2151d914927c5b6556a325c5796d
adi2120 <aditi.sharma@cumminscollege.in>
2016-09-06 14:56:14 +0530

    Create calculator.c
    This is my first file.
    Created add and subtract function.

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+}
+//this function gives additon of two numbers.
+int add(int p,int q)
+{
+return p+q;
+}
+int subtract(int p,int q)
+{
+return p-q;
+}
